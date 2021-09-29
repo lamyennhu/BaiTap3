f54dc5be41d1d9af0b72546520034a40aadec270
ENNA\AM <phyennhu0723@gmail.com>
2021-09-29 21:49:44 +0700

    add flie project

---
+++ Demo.Web/Pages/FetchData.razor.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Web.Pages
+{
+    public partial class FetchData
+    {
+        private WeatherForecast[] forecasts;
+        private readonly WeatherForecastService _weatherForecastService = new WeatherForecastService();
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await _weatherForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
---
+++ Demo.Web/Pages/FetchDataBase.cs
@@ -0,0 +1,20 @@
+﻿using Demo.Web.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Web.Pages
+{
+    public class FetchDataBase : ComponentBase 
+    {
+       private WeatherForecast[] forecasts;
+       private readonly WeatherForecastService _weatherForecastService = new WeatherForecastService();
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await _weatherForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
